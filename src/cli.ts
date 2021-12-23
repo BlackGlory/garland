16f91b63e554f63a0849132f882c09cb13531e16
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-23 11:55:59 +0000

    feat: add build

--- src/cli.ts
+++ src/cli.ts
@@ -3,19 +3,36 @@ import { program } from 'commander'
 import { build } from '@commands/build'
 import { tags } from '@commands/tags'
 
+interface IGlobalOptions {
+  tags: string
+}
+
 program
   .name(require('../package.json').name)
   .version(require('../package.json').version)
   .description(require('../package.json').description)
+  .requiredOption('--tags <filename>', 'tag definitions file')
 
 program
   .command('tags')
-  .description('列出所有标签和具有标签的对象数量(按从大到小排列)')
-  .action(() => tags())
+  .description('list all tags')
+  .action(() => {
+    const globalOptions = program.opts<IGlobalOptions>()
+
+    tags({ tagDefinitionsFilename: globalOptions.tags })
+  })
 
 program
   .command('build',)
-  .description('构建层次结构')
-  .action(() => build())
+  .description('build folder hierarchy')
+  .argument('<blueprint>', 'blueprint file')
+  .action((blueprintFilename: string) => {
+    const globalOptions = program.opts<IGlobalOptions>()
+
+    build({
+      blueprintFilename
+    , tagDefinitionsFilename: globalOptions.tags
+    })
+  })
 
 program.parse()
--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,3 +1,30 @@
-export async function build(): Promise<void> {
-  // pass
+import { readBlueprint } from '@utils/read-blueprint'
+import { readTagDefinitions } from '@utils/read-tag-definitions'
+import { calculateCondition } from '@utils/calculate-condition'
+import { link } from 'fs/promises'
+import { ensureDir } from 'extra-filesystem'
+import * as path from 'path'
+
+export async function build({
+  blueprintFilename
+, tagDefinitionsFilename
+}: {
+  blueprintFilename: string
+  tagDefinitionsFilename: string
+}): Promise<void> {
+  const blueprint = await readBlueprint(blueprintFilename)
+  const tagDefinintions = await readTagDefinitions(tagDefinitionsFilename)
+
+  for (const [targetPathname, condition] of Object.entries(blueprint)) {
+    await ensureDir(targetPathname)
+
+    for (const [sourcePathname, tags] of Object.entries(tagDefinintions)) {
+      if (calculateCondition(condition, tags)) {
+        await link(
+          sourcePathname
+        , path.join(targetPathname, path.basename(sourcePathname))
+        )
+      }
+    }
+  }
 }
